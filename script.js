81b902b5375e27b87a2fe81ee9125afd12ffe93a
yasmine <yvsminj@gmail.com>
2026-03-15 01:04:33 +0000

    fixed daily-forcast+weather api

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //current weather
 var xhr=new XMLHttpRequest();
-xhr.open("GET","https://api.openweathermap.org/data/2.5/forecast?units=metric&q=Oujda&APPID=a1118bd486a9e7a348bd31f131e2c0d5");
+xhr.open("GET","https://api.openweathermap.org/data/2.5/weather?units=metric&q=Oujda&APPID=a1118bd486a9e7a348bd31f131e2c0d5");
 
 xhr.onload=function(){
      if (xhr.status === 200) {
@@ -61,6 +61,25 @@ let humidity = hourData.main.humidity;
 hours[i].children[0].textContent = time;
 hours[i].children[2].textContent = temp + "°";
 hours[i].children[3].children[1].textContent = humidity + "%";
+}
+//daily forecast
+var days = document.querySelectorAll("aside > div");
+
+for (let i = 0; i < days.length; i++) {
+
+let dayData = data.list[i * 8];
+
+let date = new Date(dayData.dt_txt);
+let dayName = date.toLocaleDateString("en-US", { weekday: "long" });
+
+let humidity = dayData.main.humidity;
+let temp_max = parseInt(dayData.main.temp_max);
+let temp_min = parseInt(dayData.main.temp_min);
+
+days[i].children[0].textContent = dayName;
+days[i].children[1].children[1].textContent = humidity + "%";
+days[i].children[4].textContent = temp_max + "°";
+days[i].children[5].textContent = temp_min + "°";
 
 }
 }
